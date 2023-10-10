e5c08f70a0c902a3dbbc909e03337769e3712357
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-10-10 11:31:58 -0700

    add CSS for centering/etc.; update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const introText = screen.getByText(/Factorio-style Power Line Algorithm Visualizer/i);
+  expect(introText).toBeInTheDocument();
 });
